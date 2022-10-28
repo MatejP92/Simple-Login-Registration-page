a81dff06e443210e162a3214547c7cd76bf366fe
Matej~ <matej.pal@hotmail.com>
2022-10-29 00:46:33 +0200

    first commit

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 
 // connect to database
-$db_connect = mysqli_connect("localhost", "root", "", "project_login_2");
+$db_connect = mysqli_connect("localhost", "root", "", "<Your Database Name>"); 
 // if connection fails, echo this
 if(!$db_connect){
     die("database connection FAILED" . mysqli_connect_error());
